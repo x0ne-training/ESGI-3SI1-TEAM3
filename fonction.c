9550902bd43fb333877e18d5dcec411bad464b05
Damien VAURETTE <d.vaurette@myskolae.fr>
2025-10-07 19:31:10 +0200

    Amélioration de la fonction division : gestion interactive de la division par zéro
    - Remplacement du test simple par une boucle `do...while` pour redemander un diviseur valide
    - Affichage d’un message explicite en cas de tentative de division par zéro
    - Meilleure robustesse et expérience utilisateur

--- fonction.c
+++ fonction.c
@@ -31,11 +31,15 @@ float func_division() {
     float nb1, nb2;
     printf("Entrez le premier nombre : ");
     scanf("%f", &nb1);
-    printf("Entrez le deuxième nombre : ");
-    scanf("%f", &nb2);
-    if (nb2 == 0) {
-        printf("Erreur : division par zéro !\n");
-        return 0;
-    }
+
+    do {
+        printf("Entrez le deuxième nombre : ");
+        scanf("%f", &nb2);
+        if (nb2 == 0) {
+            printf("Impossible : division par zéro. Veuillez entrer un autre nombre.\n");
+        }
+    } while (nb2 == 0);
+
     return nb1 / nb2;
 }
+
