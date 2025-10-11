b9d90cd833a7e10053244578481dd2080b63527d
Amel <a.chikhaoui@myskolae.fr>
2025-10-11 10:43:04 +0200

    Lecture d'une expression utilisateur

--- calc_parser/main.c
+++ calc_parser/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
+/*int main() {
     printf("Hello, parser!\n");
+    return 0;
+}*/
+
+int main() {
+    char input[256];
+    printf("Entrez une expression : ");
+    fgets(input, sizeof(input), stdin);
+    printf("Vous avez entré : %s\n", input);
     return 0;
 }
